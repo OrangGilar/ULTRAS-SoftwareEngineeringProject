d68e74d1d3fc380866626b6a34df0d7270733739
faydzak <fairuzdzak05@gmail.com>
2026-04-29 12:59:35 +0700

    fix frontend

--- UltrasBackend/src/main/java/ULTRAS/example/UltrasBackend/UltrasBackendApplication.java
+++ UltrasBackend/src/main/java/ULTRAS/example/UltrasBackend/UltrasBackendApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UltrasBackendApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.datasource.url", "jdbc:postgresql://ep-quiet-butterfly-aouh7hhj-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+		System.setProperty("spring.datasource.username", "neondb_owner");
+		System.setProperty("spring.datasource.password", "npg_qRe4jnbEL7Vr");
 		SpringApplication.run(UltrasBackendApplication.class, args);
 	}
 
